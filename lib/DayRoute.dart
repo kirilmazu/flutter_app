e4a7f11ce21193d16071717368d4ef10dae5175c
kirilmazu <kirilm@outlook.co.il>
2019-10-16 22:27:46 +0300

    Fix long names, reformat the code

--- lib/DayRoute.dart
+++ lib/DayRoute.dart
@@ -2,42 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/Constants.dart';
 import 'package:flutter_app/LectureRoute.dart';
 
-
-class DayRoute extends StatelessWidget{
-
+class DayRoute extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Day name"),
       ),
-
       body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget> [
+          children: <Widget>[
             SingleChildScrollView(
               child: makeCards,
             )
-          ]
-      ),
+          ]),
     );
   }
-
 
   final makeCards = Container(
     child: ListView.builder(
       scrollDirection: Axis.vertical,
       shrinkWrap: true,
       itemCount: lectureCards.length,
-      itemBuilder: (BuildContext context, int index){
-        return makeGestureDetector(lectureCards[index],context);
+      itemBuilder: (BuildContext context, int index) {
+        return makeGestureDetector(lectureCards[index], context);
       },
     ),
   );
 }
 
-Widget buildContent(LectureCard card){
+Widget buildContent(LectureCard card) {
   return Container(
     child: Column(
       children: <Widget>[
@@ -53,56 +47,67 @@ Widget buildContent(LectureCard card){
   );
 }
 
-Widget buildLecturer(Lecturer lecturer){
-  if (lecturer != null && lecturer.name != null) return Container(
-    child: Row(
-      children: <Widget>[
-        Icon(Icons.person),
-        Text(lecturer.name, style: TextStyle(fontSize: 18.0)),
-      ],
-    ),
-  );
-  else return Container();
+Widget buildLecturer(Lecturer lecturer) {
+  if (lecturer != null && lecturer.name != null)
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Icon(Icons.person),
+          Text(lecturer.name, style: TextStyle(fontSize: 18.0)),
+        ],
+      ),
+    );
+  else
+    return Container();
 }
 
 //make the card clickable
-GestureDetector makeGestureDetector(LectureCard card, BuildContext context) => GestureDetector(
-  onTap: (){Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => LectureRout(card: card,)),
-  );},
-  child: Card(
-    child: Row(
-        textDirection: TextDirection.ltr,
-        children: <Widget>[
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
+GestureDetector makeGestureDetector(LectureCard card, BuildContext context) =>
+    GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => LectureRout(
+                    card: card,
+                  )
+          ),
+        );
+      },
+      child: Card(
+        child: Row(
+          textDirection: TextDirection.ltr,
+          children: <Widget>[
+            Flexible(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Column(children: <Widget>[
                     Text(card.startTime, style: TextStyle(fontSize: 15.0)),
                     Text(card.endTime, style: TextStyle(fontSize: 15.0)),
-                  ]
-                ),
-
-                Container(
-                  width: 8,
-                ),
-
-                Column(
-                    children: <Widget>[
-                      Text(card.lecture, style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold), overflow: TextOverflow.ellipsis,),
+                  ]),
+                  Container(
+                    width: 8,
+                  ),
+                  Flexible(
+                    child: Column(children: <Widget>[
+                      Text(
+                        card.lecture,
+                        maxLines: 1,
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                        overflow: TextOverflow.ellipsis,
+                      ),
                       buildContent(card),
-                    ]
-                ),
-              ],
-            ),
-          )
-        ],
+                    ]),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-  ),
-);
+    );
 
 //data for test
 List<LectureCard> lectureCards = [
@@ -121,7 +126,8 @@ List<LectureCard> lectureCards = [
     lecture: "Lecture 1",
     lecturer: new Lecturer(name: "Lecturer 1"),
     place: "Place 1",
-    description: "Some description, and this one is a very long one, and it need to take fuul page\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nthe end\n",
+    description:
+        "Some description, and this one is a very long one, and it need to take fuul page\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nv\nthe end\n",
   ),
   LectureCard(
     lectureId: 3,
@@ -136,8 +142,9 @@ List<LectureCard> lectureCards = [
     lectureId: 4,
     startTime: "11:00",
     endTime: "13:00",
-    //lecture: "Lecture with along name, for test long name as it can be in the academy, with a few lins",
-    lecture: "Lecture 3",
+    lecture:
+        "Lecture with along name, for test long name as it can be in the academy, with a few lins",
+    //lecture: "Lecture 3",
     lecturer: new Lecturer(name: "Lecturer 1"),
     place: "Place 1",
     description: "Some description",
